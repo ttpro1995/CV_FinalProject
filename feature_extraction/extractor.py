7d5bd369527e9884f9d9d37da45896fbf518c9a8
thien.thai <tthien@apcs.vn>
2016-12-25 17:01:02 +0700

    clean up

--- feature_extraction/extractor.py
+++ feature_extraction/extractor.py
@@ -2,9 +2,6 @@ import cv2
 from skimage.feature import local_binary_pattern
 import numpy as np
 
-HARRIS = 'harris'
-BLOB = 'blob'
-DOG = 'dog'
 class FeatureExtractor:
     def __init__(self):
         self.radius = 3
@@ -13,7 +10,6 @@ class FeatureExtractor:
 
     def extract(self, img):
         gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        # des = self._lbp.compute_img(img)
         lbp = local_binary_pattern(gray_img, self.n_point, self.radius, self.METHOD)
         n_bins = lbp.max() + 1
         hist, _ = np.histogram(lbp, normed=True, bins=n_bins, range=(0, n_bins))
